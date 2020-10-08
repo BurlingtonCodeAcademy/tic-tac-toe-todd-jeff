04b2c1e1d984a5f0f196ae73856d5e33f5d7e416
Jeff Van Etten <jvanetten19@gmail.com>
2020-10-08 16:32:24 -0400

    10 /8

---
+++ app.js
@@ -0,0 +1,112 @@
+let players = ['X', 'O']
+let turn = 0;
+
+let xMoves = []
+let oMoves = []
+
+let startButton = document.getElementById('start')
+let statusText = document.getElementById('status-text')
+
+let winningPatterns = ['1,2,3', '4,5,6', '7,8,9', '1,4,7', '2,5,8', '3,6,9', '1,5,9',
+    '3,5,7']
+let oMovesString = null;
+let xMovesString = null;
+
+
+startButton.addEventListener('click', startGame)
+
+// ---------------------------Our Start Game  -------------------------//
+function startGame() {
+    statusText.innerHTML = 'It\'s player' + players[0] + ' turn'
+    startButton.disabled = true;
+
+
+    //  ---------------------------Our Cell's loop -------------------------//
+    for (boxElementX of boxes) {
+
+        boxElementX.addEventListener('click', markBox)
+    }
+
+}
+//  ---------------------------Our Cells -------------------------//
+
+
+let boxes = document.getElementsByClassName('cell');
+
+
+
+
+
+
+
+function markBox(evt) {
+
+    // ---------------------------Check if vacant cell ---------------------------------//
+    if (evt.target.innerText !== '') {
+        statusText.innerText = 'Please pick a vacant cell'
+        return
+    }
+
+    // ---------------------------Set player turns ---------------------------------//       
+    evt.target.innerText = players[turn]
+
+    if (turn === 0) {                               // Set player X
+        xMoves.push(evt.target.getAttribute('id'));
+        turn = 1;
+        xMoves.sort();
+
+        xMovesString = xMoves.toString()
+
+
+    } else {                                        // Set player O
+
+        oMoves.push(evt.target.getAttribute('id'));
+        turn = 0;
+        oMoves.sort();
+        oMovesString = oMoves.toString()
+
+
+    }
+    if (checkWin(oMovesString)|| checkWin(xMovesString)) {
+        return;
+    } else {
+        statusText.innerHTML = 'It\'s player ' + players[turn] + ' turn!'      // status text update
+    }
+        
+    
+   
+}
+ // ---------------------------Check Win function ---------------------------------//   
+function checkWin(movesString) {
+    for (pattern of winningPatterns) {
+        console.log(pattern)
+        if (movesString.includes(pattern)) {
+
+            statusText.innerText = 'Winner!'
+            return true
+        }
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// ---------------------------Notes for Next time -------------------------//
+
+/* events looking at each id on each box
+
+so we need clicks and listeners
+
+this can be cloned */
